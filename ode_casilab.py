babffa76f74f7ab17b22933d01d9f42ce9dd67e3
Gautomgit <162357960+Gautomgit@users.noreply.github.com>
2024-06-10 23:59:16 +0530

    Update ode_casilab.py

--- ode_casilab.py
+++ ode_casilab.py
@@ -4,17 +4,20 @@ import numpy as np
 
 
 def solve_rk4(ode, t, y0):
-    """ ode: equation of order one as function of the form ode(t, y0)
+    """ ode: the equation as function with ode(t, y0)
         t: points at which the values are to be evaluated (as array)
-        y0: initial value 
+        y0: initial value (as array)
 
-        returns the solution as an array 
+        returns the values as an array with 'm' rows and 'n' columns,
+            where m = len(t) and n = len(y0)
             
         y = solve_rk4(ode, t, y0)
-
-        plot(t,y)
+        y[:,0],y[:,1],.......
     """
-    y = np.zeros(len(t))
+    y0 = np.array(y0)
+    y = np.zeros([len(t),len(y0)]) # initialize the array with len(t) rows and len(y0) columns
+    k1 = k2 = k3 = k4 = np.zeros(len(y0))
+    
     t0 = t[0]
     h = (t[-1] - t[0])/len(t)
     for i in range(len(t)):
@@ -23,7 +26,7 @@ def solve_rk4(ode, t, y0):
         k3 = h*ode(t0 + h/2, y0 + k2/2)
         k4 = h*ode(t0 + h, y0 + k3)
         
-        y[i] = y0
+        y[i] = y0 
         t0 += h
         y0 = y0 + (k1+2*(k2+k3)+k4)/6
 
